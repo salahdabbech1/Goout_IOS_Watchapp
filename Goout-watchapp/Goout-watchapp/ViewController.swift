68c5cc3ffd17ce91402f498ebe24a1fb44605c51
salahdabbech1 <58320947+salahdabbech1@users.noreply.github.com>
2021-11-12 09:51:23 +0100

    watchapp first commit

---
+++ Goout-watchapp/Goout-watchapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Goout-watchapp
+//
+//  Created by Apple Esprit on 12/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
